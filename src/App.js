1cb0ff240c5626a6e1251656d5a48356f9ef89cf
Reyaz-Ansari <mdreyaz7079@gmail.com>
2024-11-04 17:42:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import Services from "./components/Services/Services.js";
+import Banner from "./components/Banner/Banner.js";
+import AppStore from "./components/AppStore/AppStore.js";
+import Testimonials from "./components/Testimonials/Testimonials.js";
+import Footer from "./components/Footer/Footer.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-function App() {
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 700,
+      easing: "ease-in",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-x-hidden">
+      <Navbar />
+      <Hero />
+      <Services />
+      <Banner />
+      <AppStore />
+      <Testimonials />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,119 @@
+import React from "react";
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa6";
+import { FaWhatsappSquare } from "react-icons/fa";
+import payment from "../../assets/credit-cards.webp"
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className="text-white">
+      <div className="bg-brandDark min-h-auto">
+        <div className="container grid md:grid-cols-3 pb-5 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <a
+              href="#"
+              className="font-semibold tracking-widest text-2xl sm:text-3xl font-cursive">
+              Coffee Shop
+            </a>
+            <p className="pt-4">
+              Crafted Coffee, Cozy Vibes, Unforgettable Moments - Your Perfect
+              Espresso Escape
+            </p>
+            <a
+              href="https://www.youtube.com/channel/UC1H-a1MKEFXRiFlGNLcy7gQ"
+              target="_blank"
+              className="inline-block bg-black py-2 px-4 mt-5 text-sm rounded-full">
+              Visit Our My WebSite Here! 
+            </a>
+            {/* social links */}
+            <div className="flex items-center gap-3 mt-6">
+                  <a href="#">
+                    <FaInstagram className="text-3xl hover:text-primary duration-300" />
+                  </a>
+                  <a href="#">
+                    <FaFacebook className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                  <a href="#">
+                    <FaLinkedin className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                  <a href="#">
+                    <FaWhatsappSquare className="text-3xl hover:text-primary duration-200" />
+                  </a>
+                </div>
+          </div>
+
+          {/* Footer links */}
+          <div className="col-span-2 sm:col-span-2 grid grid-cols-2 sm:grid-cols-3 md:pl-10">
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-semibold sm:text-left mb-3">
+                Pages Links Here
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block hover:scale-105 duration-200">
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* second col links */}
+            <div className="py-8 px-4">
+              <h1 className="text-xl font-semibold sm:text-left mb-3">
+              Pages Links Here
+              </h1>
+              <ul className="space-y-3">
+                {FooterLinks.map((data, index) => (
+                  <li key={index}>
+                    <a
+                      href={data.link}
+                      className="inline-block hover:scale-105 duration-200 "
+                    >
+                      {data.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Company Address */}
+            <div className="py-8 px-4 col-span-2 sm:col-auto">
+              <h1 className="text-xl font-semibold sm:text-left mb-3">
+                Address Here
+              </h1>
+              <div>
+                <p className="mb-3">Abul Fazal, Delhi</p>
+                <p>+91 911 704 8402</p>
+                <h3 className="mt-3">Payment Here!</h3>
+                <img src={payment} alt="Payment Method" className="mt-3" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
